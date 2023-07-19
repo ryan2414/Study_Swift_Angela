ceca1e084cf2f0bb99976b890ea17f0395c5ed08
ryan2414 <jackpar@nate.com>
2023-07-19 16:23:12 +0900

    Finish Flash Chat

---
+++ Flash-Chat-iOS13-master/Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,34 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by 이승재 on 2023/07/19.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        messageBubble.clipsToBounds = true
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
